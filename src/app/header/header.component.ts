b0c8ed3f55c8e3768de797f75361bb6aa8db3e9d
rifat <rifatrahi420@gmail.com>
2024-10-23 22:16:26 +0800

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+// 2. 引入模块
+import { Component } from '@angular/core';
+
+// 3. 组件类
+@Component({
+    selector: 'app-header', // 4. 选择器：定义被组件控制的元素
+    standalone: true, // 6. 标记为独立组件（angular 14 新特性）
+    templateUrl: './header.component.html',  // 5. 模板：定义显示的内容
+    styleUrl: './header.component.css' // 7. 样式
+})
+
+// 1. 导出类
+export class HeaderComponent {}
