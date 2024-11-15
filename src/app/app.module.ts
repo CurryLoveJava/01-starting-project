e70482b7a7fe54bf6e1fcde9cea73b2ccc628676
rifat <refati.rehe@hk.yusen-logistics.com>
2024-11-15 16:45:51 +0800

    refactor: convert Task to module-based component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,35 @@
 import { NgModule } from '@angular/core';
 // 7. 导入关键组件(2次)
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { CardComponent } from './shared/card/card.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 // 3. 使用 @NgModule 装饰器
 @NgModule({
   // 4. 使用 declarations 声明，使用的所有组件
   // 非 Standalone 组件 -> declarations
   // Standalone 组件 -> imports
-  declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
   // 5. 使用 bootstrap 声明根组件
   bootstrap: [AppComponent],
   // 6. 使用 imports 导入其他的非 Standalone 模块
-  imports: [BrowserModule, TasksComponent]
+  imports: [BrowserModule, FormsModule],
 })
 
 // 1. 导出
-export class AppModule { }
+export class AppModule {}
